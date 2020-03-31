bce92c26c68f4b4ec750c23814246135470056df
Sergej Shafarenka <beworker@gmail.com>
2020-03-31 10:40:31 +0200

    Update tests

--- knot3/src/test/kotlin/de/halfbit/knot3/partial/PartialReducerTest.kt
+++ knot3/src/test/kotlin/de/halfbit/knot3/partial/PartialReducerTest.kt
@@ -5,6 +5,7 @@ import de.halfbit.knot3.Effect
 import de.halfbit.knot3.knot
 import org.junit.Test
 
+@ExperimentalStdlibApi
 class PartialReducerTest {
 
     data class State(val value: String)
@@ -12,39 +13,25 @@ class PartialReducerTest {
     data class Action(val value: String)
 
     @Test
-    fun `partial reducers are processed in registration order`() {
+    fun `partial reducers emit no actions`() {
 
         val actions = mutableListOf<Action>()
-
-        val reducer1 = object : PartialReducer<State, String, Action> {
-            override fun reduce(state: State, payload: String): Effect<State, Action> {
-                return state.copy(value = "${state.value}, $payload reducer1") + Action(
-                    "reducer1"
-                )
-            }
+        val reducers = buildList {
+            add(
+                object : PartialReducer<State, String, Action> {
+                    override fun reduce(state: State, payload: String): Effect<State, Action> {
+                        return state.only
+                    }
+                }
+            )
+            add(
+                object : PartialReducer<State, String, Action> {
+                    override fun reduce(state: State, payload: String): Effect<State, Action> {
+                        return state.only
+                    }
+                }
+            )
         }
-
-        val reducer2 = object : PartialReducer<State, String, Action> {
-            override fun reduce(state: State, payload: String): Effect<State, Action> {
-                return state.copy(value = "${state.value}, $payload reducer2") +
-                        Action("reducer2.1") + Action(
-                    "reducer2.2"
-                )
-            }
-        }
-
-        val reducer3 = object : PartialReducer<State, String, Action> {
-            override fun reduce(state: State, payload: String): Effect<State, Action> {
-                return state.copy(value = "${state.value}, $payload reducer3").only
-            }
-        }
-
-        val reducers = mutableListOf<PartialReducer<State, String, Action>>()
-            .apply {
-                this += reducer1
-                this += reducer2
-                this += reducer3
-            }
 
         val knot =
             knot<State, Change, Action> {
@@ -61,7 +48,98 @@ class PartialReducerTest {
                 actions {
                     watchAll { actions.add(it) }
                 }
+            }
 
+        val states = knot.state.test()
+        knot.change.accept(Change("one"))
+        assertThat(actions).isEmpty()
+    }
+
+    @Test
+    fun `partial reducers emit actions`() {
+
+        val actions = mutableListOf<Action>()
+        val reducers = buildList {
+            add(
+                object : PartialReducer<State, String, Action> {
+                    override fun reduce(state: State, payload: String): Effect<State, Action> {
+                        return state + Action("one")
+                    }
+                }
+            )
+            add(
+                object : PartialReducer<State, String, Action> {
+                    override fun reduce(state: State, payload: String): Effect<State, Action> {
+                        return state + Action("two") + Action("three")
+                    }
+                }
+            )
+        }
+
+        val knot =
+            knot<State, Change, Action> {
+                state {
+                    initial = State("initial")
+                }
+
+                changes {
+                    reduce { change ->
+                        reducers.dispatch(this, change.value)
+                    }
+                }
+
+                actions {
+                    watchAll { actions.add(it) }
+                }
+            }
+
+        val states = knot.state.test()
+        knot.change.accept(Change("one"))
+        assertThat(actions).containsExactly(
+            Action("one"),
+            Action("two"),
+            Action("three")
+        )
+    }
+
+    @Test
+    fun `partial reducers are processed in registration order`() {
+
+        val reducers = buildList {
+            add(
+                object : PartialReducer<State, String, Action> {
+                    override fun reduce(state: State, payload: String): Effect<State, Action> {
+                        return state.copy(value = "${state.value}, $payload reducer1").only
+                    }
+                }
+            )
+            add(
+                object : PartialReducer<State, String, Action> {
+                    override fun reduce(state: State, payload: String): Effect<State, Action> {
+                        return state.copy(value = "${state.value}, $payload reducer2").only
+                    }
+                }
+            )
+            add(
+                object : PartialReducer<State, String, Action> {
+                    override fun reduce(state: State, payload: String): Effect<State, Action> {
+                        return state.copy(value = "${state.value}, $payload reducer3").only
+                    }
+                }
+            )
+        }
+
+        val knot =
+            knot<State, Change, Action> {
+                state {
+                    initial = State("initial")
+                }
+
+                changes {
+                    reduce { change ->
+                        reducers.dispatch(this, change.value)
+                    }
+                }
             }
 
         val states = knot.state.test()
@@ -70,11 +148,6 @@ class PartialReducerTest {
         states.assertValues(
             State("initial"),
             State("initial, one reducer1, one reducer2, one reducer3")
-        )
-        assertThat(actions).containsExactly(
-            Action("reducer1"),
-            Action("reducer2.1"),
-            Action("reducer2.2")
         )
     }
 }
