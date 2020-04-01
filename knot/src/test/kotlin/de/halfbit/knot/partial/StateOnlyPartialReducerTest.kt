f7b3d5c5e2f3efd5a08916bd583c07a26d939d55
Sergej Shafarenka <beworker@gmail.com>
2020-04-01 18:30:20 +0200

    Downport to rx2 (#32)

---
+++ knot/src/test/kotlin/de/halfbit/knot/partial/StateOnlyPartialReducerTest.kt
@@ -0,0 +1,52 @@
+package de.halfbit.knot.partial
+
+import de.halfbit.knot.knot
+import org.junit.Test
+
+@ExperimentalStdlibApi
+class StateOnlyPartialReducerTest {
+
+    data class State(val value: String)
+    data class Change(val value: String)
+    data class Action(val value: String)
+
+    @Test
+    fun `partial reducers are processed in registration order`() {
+
+        val reducers = buildList {
+            add(
+                object : StateOnlyPartialReducer<State, String> {
+                    override fun reduce(state: State, payload: String) =
+                        state.copy(value = "${state.value}, $payload reducer1")
+                }
+            )
+            add(
+                object : StateOnlyPartialReducer<State, String> {
+                    override fun reduce(state: State, payload: String) =
+                        state.copy(value = "${state.value}, $payload reducer2")
+                }
+            )
+        }
+
+        val knot =
+            knot<State, Change, Action> {
+                state {
+                    initial = State("initial")
+                }
+
+                changes {
+                    reduce { change ->
+                        reducers.dispatch(this, change.value).only
+                    }
+                }
+            }
+
+        val states = knot.state.test()
+        knot.change.accept(Change("one"))
+
+        states.assertValues(
+            State("initial"),
+            State("initial, one reducer1, one reducer2")
+        )
+    }
+}
